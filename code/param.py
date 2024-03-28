60bc1e81523673a0c95a1c76427b084f9e96c38e
harshp <parasharharsh16@gmail.com>
2024-03-28 16:49:00 +0530

    added parallel processing to data loading for faster audio load

--- code/param.py
+++ code/param.py
@@ -1,7 +1,7 @@
-frame_size = 32000  # for example, 1 second of audio at 16 kHz
-sampling_rate = 16000  # assuming both files have the same sampling rate
+frame_size = 32000
+sampling_rate = 16000 
 decibel_range = [-5,5]
 dataset_path = 'data/musan'
 types_allowed = ['music', 'speech']
-sample_universe_size = 0.01
+sample_universe_size = 0.001
 n_mels=128
